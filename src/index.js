4d57d94ed8401063b49e274d21fc2496a7dba870
VizDiablo <79822447+VizDiablo@users.noreply.github.com>
2022-06-02 13:33:20 -0600

    After lesson commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", "Banana", "Coconut", ...citrus];
+const differentFruits = ["Apple", ...citrus, "Banana", "Coconut"];
+
+console.log(fruits);
+console.log(differentFruits);
+
+const fullName = {
+  fName: "James",
+  lName: "Bond"
+};
+
+const user = {
+  ...fullName,
+  id: 1,
+  username: "jamesbond007"
+};
+
+console.log(fullName);
+console.log(user);
